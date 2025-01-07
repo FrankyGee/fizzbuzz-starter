30b1a022ddcab8f892b52bdb79a442bf788d1f08
FrankyGee <frankygeorg@gmail.com>
2025-01-08 09:12:05 +1030

    Add starting outline & unit tests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,24 @@
+import sys
+
+
+def apply_fizzbuzz(number: str|int) -> str:
+    pass
+
+
+def fizzbuzz_range() -> str:
+    pass
 
 
 def main():
+    args = sys.argv[1:]
+
+    if not args:
+        # No input: Generate full range FizzBuzz
+        print(fizzbuzz_range())
+        return
+    else:
+        for number in args:
+            print(f"{number} -> {apply_fizzbuzz(number)}")
     print("HELP HELP I'M TRAPPED IN THE KERNEL")
 
 
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,9 +1,57 @@
 import unittest
+from parameterized import parameterized
 
+import validate_fizzbuzz
+from fizzbuzz import apply_fizzbuzz
+
+def test_name_func(the_test, index, params):
+    return f"{the_test.__name__} - {params.args[0]}"
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_r01_01_simple_fizz(self):
+        expected = "Fizz"
+        actual = apply_fizzbuzz(3)
+        self.assertEqual(expected, actual)
+
+    def test_r01_02_simple_buzz(self):
+        expected = "Buzz"
+        actual = apply_fizzbuzz(5)
+        self.assertEqual(expected, actual)
+
+    def test_r01_02_simple_fizzbuzz(self):
+        expected = "FizzBuzz"
+        actual = apply_fizzbuzz(15)
+        self.assertEqual(expected, actual)
+
+    def test_r01_03_simple_none(self):
+        expected = "2"
+        actual = apply_fizzbuzz(2)
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ("1", "1"),
+        ("2", "2"),
+        ("3", "Fizz"),
+        ("4", "4"),
+        ("5", "Buzz"),
+        ("6", "Fizz"),
+        ("7", "7"),
+        ("8", "8"),
+        ("9", "Fizz"),
+        ("10", "Buzz"),
+        ("11", "11"),
+        ("12", "Fizz"),
+        ("13", "13"),
+        ("14", "14"),
+        ("15", "FizzBuzz"),
+    ], name_func=test_name_func)
+    def test_r01_04_more(self, test_value, expected):
+        actual = apply_fizzbuzz(test_value)
+        self.assertEqual(expected, actual)
+
+    def test_x_from_cli(self):
+        self.assertEqual(0, validate_fizzbuzz.run_tests("../fizzbuzz.py", validate_fizzbuzz.test_cases))
+
 
 
 if __name__ == '__main__':
